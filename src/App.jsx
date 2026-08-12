111a22d82c450cf173501c61d4bffdc4dc477734
Karthik-Nakkala <karthiknakkala15@gmail.com>
2026-08-12 19:39:13 +0530

    load landing page by checking if user is not signed in platform

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,42 @@
-import { BrowserRouter,Route,Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Body from "./pages/Body";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
 import Landing from "./pages/Landing";
-import DeveloperCardStack from './components/DeveloperCardStack/DeveloperCardStack';
+import DeveloperCardStack from "./components/DeveloperCardStack/DeveloperCardStack";
 import Profile from "./components/Profile/Profile";
 import EditDeveloperProfileModal from "./components/Profile/EditDeveloperProfileModal";
 import Connections from "./components/Connections";
 import ConnectionRequests from "./components/ConnectionRequests";
+import { useSelector } from "react-redux";
 
 function App() {
+  const user = useSelector((store) => store.user);
 
   return (
-    <div className="bg-[#070B18]">
-       <BrowserRouter basename="/">
-        <Routes>
-          <Route path="/" element={<Body/>}>
-          <Route index element={<DeveloperCardStack/>}></Route>
-          <Route path="/profile" element={<Profile/>}></Route>
-          <Route path="/profile-edit" element={<EditDeveloperProfileModal/>}></Route>
-          <Route path="/connections" element={<Connections/>}></Route>
-          <Route path="/requests" element={<ConnectionRequests/>}></Route>
-          </Route>
-          <Route path="/login" element={<Login/>}/>
-          <Route path="/signup" element={<Signup/>}/>
-          <Route path="/landing" element={<Landing/>}/>
-        </Routes>
-       </BrowserRouter>
-    </div>  
-  )
+    <BrowserRouter basename="/">
+      {!user ? (
+        <Landing />
+      ) : (
+        <div className="bg-[#070B18]">
+          <Routes>
+            <Route path="/" element={<Body />}>
+              <Route index element={<DeveloperCardStack />}></Route>
+              <Route path="/profile" element={<Profile />}></Route>
+              <Route
+                path="/profile-edit"
+                element={<EditDeveloperProfileModal />}
+              ></Route>
+              <Route path="/connections" element={<Connections />}></Route>
+              <Route path="/requests" element={<ConnectionRequests />}></Route>
+            </Route>
+            <Route path="/login" element={<Login />} />
+            <Route path="/signup" element={<Signup />} />
+          </Routes>
+        </div>
+      )}
+    </BrowserRouter>
+  );
 }
 
 export default App;
--- src/pages/Body.jsx
+++ src/pages/Body.jsx
@@ -19,7 +19,7 @@ const Body = () => {
       dispatch(addUser(user.data));
     } catch (err) {
       if (err.status == 401) {
-        navigate("/landing");
+        navigate("/login");
       }
       console.log(err);
     }
