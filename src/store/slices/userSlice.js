1bbd021d3774bb147ad92eb17d577245923e541e
Karthik-Nakkala <karthiknakkala15@gmail.com>
2026-08-25 17:31:20 +0530

    remove addPremium reducer action from user redux slice

--- src/store/slices/userSlice.js
+++ src/store/slices/userSlice.js
@@ -1,20 +1,17 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-const userSlice=createSlice({
-    name:"user",
-    initialState:null,
-    reducers:{
-        addUser:(state,action)=>{
-            return action.payload;
-        },
-        addPremium:(state,action)=>{
-            state.isPremium=action.payload;
-        },
-        removeUser:()=>{
-            return null;
-        }
-    }
+const userSlice = createSlice({
+  name: "user",
+  initialState: null,
+  reducers: {
+    addUser: (state, action) => {
+      return action.payload;
+    },
+    removeUser: () => {
+      return null;
+    },
+  },
 });
 
-export const {addUser,addPremium,removeUser}=userSlice.actions;
+export const { addUser, removeUser } = userSlice.actions;
 export default userSlice.reducer;
